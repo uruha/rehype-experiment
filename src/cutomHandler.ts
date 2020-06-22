1b0b95e719e2877700e1dd336d54f042bc8038e1
chiba-kotaro <chiba-kotaro@PC-0001199.local>
2020-06-22 17:30:28 +0900

    update: attach types (wip)

--- src/cutomHandler.ts
+++ src/cutomHandler.ts
@@ -1,38 +1,71 @@
-const snakeToCamel = (str) => str.replace(
+import { Node } from 'unist';
+import { VFile } from 'vfile';
+
+export type CustomAttr = {
+    attr: string
+}
+
+interface DoneCallbackType {
+    (argv?: Error): Promise<void | Error>;
+}
+interface AppendCustomAttr {
+    (node: Node | ExNode, opt: CustomAttr): Boolean
+}
+interface CoverNodes {
+    (
+        converter: AppendCustomAttr,
+        node: Node,
+        parentNode: Node | null,
+        index: number,
+        opt: CustomAttr
+    ): void
+}
+interface ExNode extends Node {
+    properties?: string[]
+}
+
+const snakeToCamel = (str: string) => str.replace(
     /([-_]\w)/g,
     (group) => group[1].toUpperCase()
 );
 
-const appendCustomAttr = (node, opt) => {
+const appendCustomAttr: AppendCustomAttr = (node, opt) => {
     if(
         node.type === 'element' &&
         node.tagName === 'a' &&
+        // @ts-ignore
         node.properties.className &&
+        // @ts-ignore
         node.properties.className.includes(opt.attr)
     ) {
         const customAttr = snakeToCamel(opt.attr);
+        // @ts-ignore
         node.properties[customAttr] = true;
         return true;
     }
     return false;
 };
 
-const cover = (converter, node, parentNode, index, opt) => {
+const cover: CoverNodes = (
+    converter, node, parentNode, index, opt
+): void => {
     if(converter(node, opt)) return;
     if(!node.children) return;
 
+    // @ts-ignore
     for(let i = 0; i < node.children.length; i++) {
+        // @ts-ignore
         cover(converter, node.children[i], node, i, opt);
     }
 };
 
-const handler = (opt) => {
+export const customAttrHandler = (opt: CustomAttr) => {
     if(!opt) {
         console.warn('This handler need "attr" property !');
-        return (node, vfile, done) => done();
+        return (node: Node, vfile: VFile, done: DoneCallbackType) => done();
     }
 
-    return (node, vfile, done) => {
+    return (node: Node, vfile: VFile, done: DoneCallbackType) => {
         try {
             cover(appendCustomAttr, node, null, 0, opt);
             done();
--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,15 @@ console.log(hastTree);
 import { schema } from './sanitize';
 import sanitize from 'rehype-sanitize';
 
+// custom handler
+import { customAttrHandler } from './cutomHandler';
+
 
 rehype()
   .data('settings', {fragment: true})
   .use(sanitize, schema)
-//   .use(handler, { attr: 'page-scroll' })
+  // @ts-ignore
+  .use(customAttrHandler, { attr: 'page-scroll' })
   .process(fs.readFileSync(`${__dirname}/raw.html`), function(err, file) {
     if (err) throw err
     console.log(String(file))
